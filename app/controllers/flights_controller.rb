729a25c9f636a1023b51f10cbf36d51e94c5a6a2
C4mz0r <aut0x@hotmail.com>
2016-07-12 21:47:26 -0600

    Index view shows the airport dropdowns

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,2 +1,10 @@
 class FlightsController < ApplicationController
+  
+  def index
+    @flights = Flight.all
+    #@flight_options =
+    @fromAirports = Airport.all.map {|a| [a.name, a.id]}
+    @toAirports = Airport.all.map {|a| [a.name, a.id]}
+  end
+  
 end
