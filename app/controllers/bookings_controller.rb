8f8f3dc8761ffbd24334eb8fa39f695808caff8e
C4mz0r <aut0x@hotmail.com>
2016-07-14 10:09:47 -0600

    Improve passing of passenger info to bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,10 +1,7 @@
 class BookingsController < ApplicationController
   def new
     @flight = params[:flight]
-    
-    # Naming is based on the arguments in the hidden field call.
-    # This feels wrong, need to find out if there is a better way to do it.
-    @passengers = params[:passengers][:passengers]
+    @passengers = params[:passengers]
     
   end
 end
