0a89d136b24280af8395280bc5e26875dcc7a9c6
C4mz0r <aut0x@hotmail.com>
2016-07-13 23:02:01 -0600

    Add simple booking controller and able to pass parameters to the new method

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -5,12 +5,15 @@ class FlightsController < ApplicationController
     @fromAirports = Airport.getSourceAirports
     @toAirports = Airport.getDestinationAirports 
     @flightDates = Flight.formatFlightDates(Flight.getFlightDates)
+    
+    # Used in the view    
+    @passengers = params[:num_tickets]
      
     # Used in results below, but also in the view to keep these
     # options selected after page refresh
     @selectedFromAirport = params[:from_code]
     @selectedToAirport = params[:to_code]
-    
+        
     @results = Flight.searchForFlights(@selectedFromAirport, @selectedToAirport, params[:date])
     
       
