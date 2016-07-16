e7c7f675c722fa34201592610ebd035c9d9454b7
C4mz0r <aut0x@hotmail.com>
2016-07-16 13:47:57 -0600

    Select the top radio button by default in the search results

--- app/helpers/flights_helper.rb
+++ app/helpers/flights_helper.rb
@@ -1,2 +1,7 @@
 module FlightsHelper
+  # Called from the view on a radio button, returns true if the current index is 0
+  # This makes the radio button selected
+  def selectIfTopButton(index)
+    index == 0 ? true : false
+  end
 end
