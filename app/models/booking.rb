88f2b3c332cbe5f8f89db99895cf7daeb8422f82
C4mz0r <aut0x@hotmail.com>
2016-07-15 20:19:46 -0600

    Removed some stray commented code from models and put forms into partials

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,7 +1,5 @@
 class Booking < ActiveRecord::Base
-  belongs_to :flight
-  #belongs_to :passenger  
-  #has_many :tickets
+  belongs_to :flight  
   has_many :passenger_bookings
   has_many :passengers, through: :passenger_bookings
   accepts_nested_attributes_for :passengers
--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,5 +1,4 @@
-class Passenger < ActiveRecord::Base
-  #belongs_to :bookings
+class Passenger < ActiveRecord::Base  
   has_many :passenger_bookings
   has_many :bookings, through: :passenger_bookings
   has_many :flights, through: :bookings
