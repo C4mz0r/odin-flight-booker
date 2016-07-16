8ec0e508469c2ba4fd53383546fd7d3ef89a2680
C4mz0r <aut0x@hotmail.com>
2016-07-16 16:57:49 -0600

    Put page titles in the header

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -20,7 +20,7 @@ class BookingsController < ApplicationController
   end
   
   def show
-    @booking = Booking.find(params[:id])
+    @booking = Booking.find(params[:id])    
   end
   
     private
